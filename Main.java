f46ca0171ef404fd160c92a7430e579b35feb1e2
Mufasa 333 <>
2022-09-12 20:15:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+class Main {
+  public static void main(String[] args) {
+
+         double age = 15.5;
+        System.out.println("You are " + age + " years old!");
+        System.out.println(2 + 2);
+        System.out.println(4-14);
+        System.out.println(6*7);
+        System.out.println(24/6);
+        //does not work, need to make one number or both a decimal 
+        System.out.println(25/6);
+        // integer division 
+        System.out.println(25.0/6);
+        
+        System.out.println(25%6);
+
+        //print What is your age and then the number
+       System.out.println("Hi there " + 3);
+        //output 2 * 2 = 4
+        System.out.println("2 * 2 = " + 2 * 2);
+
+          //Outputs "Hello world"
+        System.out.print("Hello");
+        System.out.println(" World");
+        //Bonus: What if we want to skip lines but we want to reduce
+        //the amount of code we want to use
+
+        System.out.println("Hello\nWorld");
+
+        //we use the \n to create lines
+        //or if we want to create space (tabs) between words
+
+        System.out.println("Hello\tworld");
+
+
+    
+  }
+}
